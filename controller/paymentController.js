8b8073219d55049c014be55d208416876c5c146f
aastha333 <aasthabhardwaj333@gmail.com>
2022-04-11 17:03:01 +0530

    commit

--- controller/paymentController.js
+++ controller/paymentController.js
@@ -8,7 +8,7 @@ const getPayment=async function(req,res){
     try
     {
         await Payment.find().then((result)=>{
-            if(result.length>0)
+            if(result>0)
                 res.json(result);
             else
                 res.json({result:"No Data Exist!"});
@@ -114,7 +114,7 @@ const deletePayment=async(req,res)=>{
     }
 const makePayment=async(req,res)=>{
     try{
-        await Order.findOneAndUpdate({_id:ObjectId(req.query.orderId),customerId:req.query.customerId}).then(async(data)=>{
+        await Order.findOneAndUpdate({_id:ObjectId(req.query.orderId)},{customerId:req.query.customerId}).then(async(data)=>{
             if(data&&data.status=='Payment Incomplete'){
                 await Payment.findOne({paymentId:req.query.paymentId},{customerId:req.query.customerId}).then((result)=>{
                     if(result){ 
